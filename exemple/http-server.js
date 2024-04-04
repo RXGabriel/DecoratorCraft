90ad96766a1244af661c7ff0974271907a025bfa
RXGabriel <gabriel2005rf@gmail.com>
2024-04-04 10:31:47 -0300

    feat: Add HTTP interceptor setup to server initialization

--- exemple/http-server.js
+++ exemple/http-server.js
@@ -1,8 +1,9 @@
 import http from 'http'
-
+import { InjectHttpInterceptor } from './../index.js'
 function handleRequest(request, response) {
     response.end('Hello World!')
 }
+InjectHttpInterceptor()
 const server = http.createServer(handleRequest)
 const port = 3000
 server.listen(port, () => {
---
+++ index.js
@@ -0,0 +1,4 @@
+import { InjectHttpInterceptor } from './src/agent.js'
+export {
+    InjectHttpInterceptor
+}
